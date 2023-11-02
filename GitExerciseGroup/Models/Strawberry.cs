e7761fdab57c4fe74f5f1604576a18d340c2f7af
DESKTOP-NS8HAV3 <may_aj@outlook.com>
2023-11-02 09:35:07 +0100

    added strawberry class

---
+++ GitExerciseGroup/Models/Strawberry.cs
@@ -0,0 +1,11 @@
+﻿namespace GitExerciseGroup.Models
+{
+    public class Strawberry
+    {
+        public string Color { get; set; } = string.Empty;
+        public Strawberry()
+        {
+            
+        }
+    }
+}
