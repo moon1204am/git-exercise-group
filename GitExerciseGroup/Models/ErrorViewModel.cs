b858ea22780dc52128019e6c3c76099bc13ceb13
Sammy Wong <sammywong528@outlook.com>
2023-11-02 10:23:53 +0100

    Change back to before

--- GitExerciseGroup/Models/ErrorViewModel.cs
+++ GitExerciseGroup/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace GitExerciseGroup.Models;
-
-public class ErrorViewModel
+namespace GitExerciseGroup.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
